4b0bb2552ebc3682d6d0b6a8cf0b177a2992670c
JKilgallen <jkilgallen@protonmail.com>
2021-09-14 22:34:24 +0100

    Modified player class to enforce typing for move method.

--- src/player.py
+++ src/player.py
@@ -1,9 +1,15 @@
 from abc import ABCMeta, abstractmethod
+from tokenize import String
+from typing import Tuple, final, Union
 
 class Player(metaclass = ABCMeta):
     """The Player class is an abstract base class from which all
        game playing AIs must inherit."""
     
+    @final
+    def __init__(self, index) -> None:
+        self.index = index
+
     @abstractmethod
-    def move(self, bids, dice):
+    def move(self, bids, dice) -> Union[str, Tuple[int, int]]:
         """Make a move based on dice value and bids in round."""
