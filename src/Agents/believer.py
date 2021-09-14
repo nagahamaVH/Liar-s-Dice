382b4f5ce9b57e1f1de274bbd083361c56e57066
JKilgallen <jkilgallen@protonmail.com>
2021-09-14 22:35:03 +0100

    Modified game playing agents to account for specification changes.

--- src/Agents/believer.py
+++ src/Agents/believer.py
@@ -3,9 +3,9 @@ from player import Player
 class Believer(Player):
     """The Believer class implements a player who always believes the previous player to be exactly right."""
 
-    def move(self, bid, dice):
+    def move(self, bids, dice):
         """The move method returns 'Spot on' if bid is not empty and '1,1' otherwise."""
-        if bid:
+        if bids:
             return 'Spot on'
         else:
-            return '1,1'
+            return (1,1)
--- src/Agents/skeptic.py
+++ src/Agents/skeptic.py
@@ -4,9 +4,9 @@ class Skeptic(Player):
     """The skeptic class implements a player who always believes the 
        preceding player is lying."""
 
-    def move(self, bid, dice):
-        """The move method returns 'Liar' if bid is not empty and '1,1' otherwise."""
-        if bid:
+    def move(self, bids, dice):
+        """The move method returns ('Liar') if bids is not empty and (1,1) otherwise."""
+        if any(bids):
             return 'Liar'
         else:
-            return '1,1'
+            return (1,1)
